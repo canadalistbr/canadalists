4d6396225d1ecbae0329f7cf71d1bc61a9acacac
Vitor Correia <vitorbaziliogcorreia@gmail.com>
2022-05-19 21:12:30 -0400

    initial setup next.js

---
+++ client/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>
+          Canada Lists
+        </title>
+      </Head>
+      Canada lists
+    </div>
+  )
+}
+
+export default Home
