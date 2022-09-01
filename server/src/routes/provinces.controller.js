df67167397d73af62a069e25c026e60659f97106
leo <lstudart@pyx4.com>
2022-09-01 17:43:07 -0400

    feat:add getProvincesbById controller

--- server/src/routes/provinces.controller.js
+++ server/src/routes/provinces.controller.js
@@ -1,10 +1,16 @@
 const { Provinces } = require("../models/provinces.model");
 
 function getAllProvinces(req, res) {
-  const allProvinces = Provinces.getAllProvinces()
+  const allProvinces = Provinces.getAllProvinces();
   return res.status(200).json(allProvinces);
+}
+
+function getProvinceById(req, res) {
+  const province = Provinces.getProvinceById(req.body.id);
+  return res.status(200).json(province);
 }
 
 module.exports = {
   getAllProvinces,
+  getProvinceById
 };
