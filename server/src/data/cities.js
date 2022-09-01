c3923b978dfafe7cbb3371c777b300ab5100c8a8
leo <lstudart@pyx4.com>
2022-09-01 17:38:21 -0400

    feat/add cities data

---
+++ server/src/data/cities.js
@@ -0,0 +1,9 @@
+//TODO: Add rest of provinces and move it into DB later on
+//TODO: Check if minimum wages are up to date.
+const cities =[{
+  id:"1",
+  name: "Montréal",
+  province: "Quebec"
+}];
+
+module.exports = cities;
