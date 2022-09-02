4d08bcdb8984d4cfcbcabd9dd9a75ee55586a6e9
leo <lstudart@pyx4.com>
2022-09-02 12:34:22 -0400

    fetch provinces on home page

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,13 @@
 const express = require("express");
-
+const { getAllProvinces } = require("./routes/provinces.controller");
 const provincesRoutes = require("./routes/provinces.route");
 const citiesRoutes = require("./routes/cities.route");
 
 
 const app = express();
 
+app.get("/", getAllProvinces)
 app.use(express.json())
-
 app.use(provincesRoutes)
-app.use(citiesRoutes)
 
 module.exports = app
