4fbb3045a7ea373513e0ba79454f0d4b6b5b5eeb
vcorreia <vcorreia@pyx4.com>
2022-09-01 21:38:00 -0400

    adding provinces list and province page

---
+++ client/pages/provinces/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+import type { NextPage } from 'next'
+const Provinces: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>
+          Provinces List
+        </title>
+      </Head>
+      Provinces List
+    </div>
+  )
+}
+
+export default Provinces
