d2f558e630439517b466912453b0055304f04666
leo <lstudart@pyx4.com>
2022-09-02 15:19:18 -0400

    feat: add route to getProvinceByName

--- server/src/routes/provinces.route.js
+++ server/src/routes/provinces.route.js
@@ -1,8 +1,12 @@
-const { getAllProvinces } = require("./provinces.controller");
+const {
+  getAllProvinces,
+  getProvinceByName,
+} = require("./provinces.controller");
 const express = require("express");
 
 const provincesRoutes = express.Router();
 
 provincesRoutes.get("/", getAllProvinces);
+provincesRoutes.get("/:provinceName", getProvinceByName);
 
 module.exports = provincesRoutes;
