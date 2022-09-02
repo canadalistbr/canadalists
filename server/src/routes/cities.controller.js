b8dcd7b73c24238ddddac47bca3d740fc574d7a4
leo <lstudart@pyx4.com>
2022-09-02 15:19:33 -0400

    chore: update getCityByName controller to return an error if no province is found

--- server/src/routes/cities.controller.js
+++ server/src/routes/cities.controller.js
@@ -6,12 +6,14 @@ function getAllCities(req, res) {
 }
 
 function getCityByName(req, res) {
-  const cityName = req.params.cityName
-  const city = Cities.getCityByName(cityName)
+  const cityName = req.params.cityName;
+  const city = Cities.getCityByName(cityName);
   if (!city) {
-    return res.redirect("/cities")
+    res.status(404).json({
+      error: "Sorry, this city does not exist",
+    });
   }
-  return res.status(200).json(city)
+  return res.status(200).json(city);
 }
 
 module.exports = {
