a10c138c899a5a4a85d72813a6e6f461532c36c2
leo <lstudart@pyx4.com>
2022-09-02 15:22:27 -0400

    util: add normalize util function

--- server/src/utils/removeAccents.js
+++ server/src/utils/removeAccents.js
@@ -2,7 +2,11 @@ function removeAccents(string) {
     return string.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
 }
 
+function normalizeEntityName(string) {
+    return removeAccents(string).toLowerCase()
+}
 
 module.exports = {
-    removeAccents
+    removeAccents,
+    normalizeEntityName
 }
