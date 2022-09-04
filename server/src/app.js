5250eb3ac5cc75d3ed4a1c213eb4126ccc596de2
leo <lstudart@pyx4.com>
2022-09-04 19:52:48 -0400

    add config for database

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,6 @@ const { getAllProvinces } = require("./routes/provinces.controller");
 const provincesRoutes = require("./routes/provinces.route");
 const citiesRoutes = require("./routes/cities.route");
 
-
 const app = express();
 
 app.use(express.json())
