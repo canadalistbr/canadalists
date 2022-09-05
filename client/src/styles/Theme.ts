d8492cfc88ac55520e56ff62eabd0757e568fbf7
vcorreia <vcorreia@pyx4.com>
2022-09-05 13:47:48 -0400

    (styles): add Theme

---
+++ client/src/styles/Theme.ts
@@ -0,0 +1,10 @@
+// palette color https://www.color-hex.com/color-palette/58700
+export const Theme = {
+  black: '#000000',
+  white: '#ffffff',
+  red: '#ff0000',
+  lightGray: '#dfdfdf',
+  gray: '#aeaeae',
+  pink: '#ff8181',
+  modile: '768px'
+}
