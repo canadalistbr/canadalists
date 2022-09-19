0a66cdfd411433e7f25fe455120224980bdf9d49
leo <lstudart@pyx4.com>
2022-09-19 10:53:59 -0400

    test: add try catch into loadProvinces controller

--- serverDDD/src/presentation/controllers/provinces/load-provinces-controller.spec.ts
+++ serverDDD/src/presentation/controllers/provinces/load-provinces-controller.spec.ts
@@ -1,4 +1,4 @@
-import { ok } from "../../helpers/http-helper";
+import { ok, serverError } from "../../helpers/http-helper";
 import { LoadProvincesController } from "./load-provinces-controller";
 import {
   LoadProvinces,
@@ -59,5 +59,12 @@ describe("LoadProvincesController", () => {
     const { sut } = makeSut();
     const httpResponse = await sut.handle({});
     expect(httpResponse.statusCode).toEqual(ok(makeFakeProvinces()).statusCode);
+  });
+
+  it("should return 500 if LoadProvinces throw", async () => {
+    const { sut, loadProvincesStub } = makeSut();
+    jest.spyOn(loadProvincesStub, 'load').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error)))
+    const httpResponse = await sut.handle({});
+    expect(httpResponse).toEqual(serverError(new Error));
   });
 });
