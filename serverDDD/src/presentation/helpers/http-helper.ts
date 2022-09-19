04d2f76456b45e50cf909503621edc70ad34656a
leo <lstudart@pyx4.com>
2022-09-19 10:53:17 -0400

    chore: add serverError http helper

--- serverDDD/src/presentation/helpers/http-helper.ts
+++ serverDDD/src/presentation/helpers/http-helper.ts
@@ -1,8 +1,16 @@
+import { ServerError } from "../errors/server-errors";
 import { HttpResponse } from "../protocols";
 
 export function ok(data: any): HttpResponse {
   return {
     statusCode: 200,
     body: data,
+  };
+}
+
+export function serverError(error: Error): HttpResponse {
+  return {
+    statusCode: 500,
+    body: new ServerError(error.stack),
   };
 }
