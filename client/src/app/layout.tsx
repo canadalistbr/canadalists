2b5ef0bbc3e802e0394681d85a9223248d625d41
leo <lstudart@pyx4.com>
2023-04-06 10:42:01 -0400

    chore: use Head in layout and remove p tag

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,22 +1,23 @@
 // These styles apply to every route in the application
-import './globals.css';
+import Head from 'next/head'
+import './globals.css'
 
 export const metadata = {
   title: 'Canada List',
-  description: 'Move to Canada',
+  description: 'Move to Canada'
 }
 
-
 export default function RootLayout({
-  children,
+  children
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body>
-        <p>{children}</p>
-      </body>
+      <Head>
+        <title>Canada List</title>
+      </Head>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
