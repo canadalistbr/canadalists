d21506ab3955053ecd7edc08a7f9f7b28cbab6e2
leo <lstudart@pyx4.com>
2023-04-16 13:45:26 -0400

    feat: add MaxWidthWrapper on Province Page

--- client/src/app/provinces/page.tsx
+++ client/src/app/provinces/page.tsx
@@ -1,20 +1,22 @@
 import { Card } from 'app/components/Card/Card'
+import MaxWidthWrapper from 'app/components/MaxWidthWrapper'
 import Link from 'next/link'
 import { ProvincesData } from '../../../data/provinces'
 function Provinces() {
   return (
-    <div className="grid relative gap-4 grid-cols-[repeat(auto-fill,minmax(350px,1fr))]">
-      {ProvincesData.map((province) => (
-        <Link key={province.id} href={`/provinces/${province.slug}`}>
-          <Card
-            slug={province.slug}
-            key={province.id}
-            title={province.name}
-            image="/img/montreal.png"
-          />
-        </Link>
-      ))}
-    </div>
+    <MaxWidthWrapper>
+      <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(350px,1fr))]">
+        {ProvincesData.map((province) => (
+          <Link key={province.id} href={`/provinces/${province.slug}`}>
+            <Card
+              slug={province.slug}
+              title={province.name}
+              image="/img/montreal.png"
+            />
+          </Link>
+        ))}
+      </div>
+    </MaxWidthWrapper>
   )
 }
 
