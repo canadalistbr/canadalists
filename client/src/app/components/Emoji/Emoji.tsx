55c89e7ab698c3ab952c36c9886f65a9158580af
leo <lstudart@pyx4.com>
2023-04-24 19:43:46 -0400

    feat: add emoji component

---
+++ client/src/app/components/Emoji/Emoji.tsx
@@ -0,0 +1,33 @@
+import { PropsWithChildren, ReactNode } from 'react'
+
+export type EmojiProps = {
+  label: string
+  symbol: ReactNode
+}
+
+export function Emoji(props: EmojiProps) {
+  return (
+    <span
+      className="emoji"
+      role="img"
+      aria-label={props.label ? props.label : ''}
+      aria-hidden={props.label ? 'false' : 'true'}
+    >
+      {props.symbol}
+    </span>
+  )
+}
+
+export type LabelWithEmojiProps = PropsWithChildren<{
+  emojiProps: EmojiProps
+}>
+
+export function LabelWithEmoji(props: LabelWithEmojiProps) {
+  const { emojiProps, children } = props
+  return (
+    <div className="flex gap-2">
+      <Emoji {...emojiProps} />
+      <span>{children}</span>
+    </div>
+  )
+}
