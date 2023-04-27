51d2fe9e0e3718fba6ee069697f6783480fb3e3f
leo <lstudart@pyx4.com>
2023-04-27 10:19:15 -0400

    chore: install tailwind debug screen

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className='debug-screens'>
         <div>{children}</div>
       </body>
     </html>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,19 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}', // Note the addition of the `app` directory.
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './compo nents/**/*.{js,ts,jsx,tsx}',
 
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
+    debugScreens: {
+      position: ['bottom', 'left'],
+    },
     extend: {
       gridTemplateColumns: {
-        fluid: "min(calc(1200px + 32px * 2))"
+        fluid: 'min(calc(1200px + 32px * 2))'
       }
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-debug-screens')]
 }
