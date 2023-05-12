012f1d932eeb714dc8969d4e78a9a02c9a850e6e
leo <lstudart@pyx4.com>
2023-05-12 11:16:45 -0400

    feat: replace mock provinces data by provinces api

--- src/app/provinces/page.tsx
+++ src/app/provinces/page.tsx
@@ -1,18 +1,42 @@
 import { Card } from 'app/components/Card/Card'
+import { CustomTag } from 'app/components/CustomTag'
 import MaxWidthWrapper from 'app/components/MaxWidthWrapper'
+import { Province } from 'app/lib'
 import Link from 'next/link'
-import { ProvincesData } from '../../../data/provinces'
-function Provinces() {
+
+async function getProvinces(): Promise<Provinces> {
+  const res = await fetch(`${process.env.BASE_URL}/api/provinces/v1/provinces`)
+  return res.json()
+}
+
+export type Provinces = {
+  provinces: Province[]
+}
+
+async function Provinces() {
+  const { provinces } = await getProvinces()
+
   return (
     <MaxWidthWrapper>
       <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(350px,1fr))]">
-        {ProvincesData.map((province) => (
+        {provinces.map((province) => (
           <Link key={province.id} href={`/provinces/${province.name}`}>
             <Card
               slug={province.slug}
               title={province.name}
               image="/img/montreal.png"
-            />
+            >
+              <div className="">
+                <div className="flex items-center ">
+                  <span className='inline-block' >Top Cities:</span>
+                  <div className="flex gap-2">
+                    {province.topCities.map((city, index) => (
+                      <CustomTag key={index}>{city}</CustomTag>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </Card>
           </Link>
         ))}
       </div>
@@ -21,3 +45,4 @@ function Provinces() {
 }
 
 export default Provinces
+
