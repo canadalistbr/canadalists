bd4561042e3f4b2b5aaa5662980ee77162e6d66a
Pyx4 <leost3@github.com>
2023-05-13 14:02:40 -0400

    feat: fetch data and display

--- src/app/provinces/page.tsx
+++ src/app/provinces/page.tsx
@@ -1,17 +1,32 @@
+import { Province } from '@prisma/client'
 import { Card } from 'app/components/Card/Card'
 import MaxWidthWrapper from 'app/components/MaxWidthWrapper'
 import Link from 'next/link'
-import { ProvincesData } from '../../../data/provinces'
-function Provinces() {
+export type Provinces = {
+  provinces: Province[]
+}
+
+export async function getProvinces(): Promise<Provinces> {
+  const res = await fetch(`${process.env.BASE_URL}/api/provinces`, {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'GET'
+  })
+  return res.json()
+}
+async function Provinces() {
+  const { provinces } = await getProvinces()
+
   return (
     <MaxWidthWrapper>
       <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(350px,1fr))]">
-        {ProvincesData.map((province) => (
+        {provinces.map((province) => (
           <Link key={province.id} href={`/provinces/${province.name}`}>
             <Card
               slug={province.slug}
               title={province.name}
-              image="/img/montreal.png"
+              image={province.imageUrl}
             />
           </Link>
         ))}
