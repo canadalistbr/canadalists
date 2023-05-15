5e69968d138e1da3cc77cb3c4caa05c2954f0432
Pyx4 <leost3@github.com>
2023-05-15 16:41:34 -0400

    refactor: remove name from overview

---
+++ prisma/migrations/20230515201005_remove_name_from_overview/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `ProvinceOverview` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ProvinceOverview" DROP COLUMN "name";
