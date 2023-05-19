a64c875b2cfffe04f2c50f24764a52327b8d6fc7
Pyx4 <leost3@github.com>
2023-05-19 18:46:38 -0400

    feat: fetch data to immigration and city and update sideCards with provinces data

---
+++ src/app/provinces/[name]/components/ImmigrationPrograms.tsx
@@ -0,0 +1,25 @@
+import { Immigration } from '@prisma/client'
+import { Card } from 'app/components/Card/Card'
+import Link from 'next/link'
+
+export type ImmigrationProgramsType = {
+  immigrationPrograms: Immigration[]
+}
+
+export function ImmigrationPrograms({
+  immigrationPrograms
+}: ImmigrationProgramsType) {
+  return (
+    <div className="grid grid-cols-[repeat(auto-fill,minmax(550px,1fr))]">
+      {immigrationPrograms.map((program) => (
+        <Link key={program.id} href={`/immigration_programs/${program.name}`}>
+          <Card
+            slug={program.name}
+            title={`${program.name}, ${program.name}`}
+            image="/img/montreal.png"
+          />
+        </Link>
+      ))}
+    </div>
+  )
+}
