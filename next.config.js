2afff11cd8d7d83ac56248a3afeabb7d29123425
Pyx4 <leost3@github.com>
2023-05-25 22:46:58 -0400

    chore: update next version to 13.4 (stable)

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
-    domains: ['xdcgqdmfytknfwfiixza.supabase.co'],
-  },
-};
+    domains: ['xdcgqdmfytknfwfiixza.supabase.co']
+  }
+}
 
 module.exports = nextConfig
