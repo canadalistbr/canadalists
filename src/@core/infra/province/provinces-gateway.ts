ab9ccf39971fefd59b45303dbee6cd8c1230b8b9
Pyx4 <leost3@github.com>
2023-06-01 17:21:49 -0400

    refactor: add assertion to provinces gateway

--- src/@core/infra/province/provinces-gateway.ts
+++ src/@core/infra/province/provinces-gateway.ts
@@ -5,10 +5,19 @@ import { AxiosInstance } from 'axios'
 export class ProvincesHttpGateway implements ProvincesGateway {
   constructor(private readonly http: AxiosInstance) {}
   async findById(id: string): Promise<Province> {
-    return (await this.http.get(`${process.env.BASE_URL}/api/provinces/${id}`))
-      .data
+    const res = await this.http.get(
+      `${process.env.BASE_URL}/api/provinces/${id}`
+    )
+    if (!res.data) {
+      throw Error('Something went Wrong')
+    }
+    return res.data
   }
   async findAll(): Promise<Province[]> {
-    return (await this.http.get(`${process.env.BASE_URL}/api/provinces`)).data
+    const res = await this.http.get(`${process.env.BASE_URL}/api/provinces`)
+    if (!res.data) {
+      throw Error('Something went Wrong')
+    }
+    return res.data
   }
 }
