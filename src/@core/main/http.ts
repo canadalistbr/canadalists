dc1cab67bbe410e02d3e1cfd0801b3e98daa8855
Leonardo Studart <leost3@gmail.com>
2023-10-13 10:32:09 -0400

    refactor: use BASE_URL for data fetching in ProvincesHttpGateway (#61)
    Co-authored-by: Leo <lstudart@pyx4.com>

--- src/@core/main/http.ts
+++ src/@core/main/http.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosHttp = axios.create({
-  baseURL: process.env.BASE_URL_BACKEND,
+  baseURL: process.env.BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
