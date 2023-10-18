a94a15ec492c5dc397a6402747b18531cfda1f54
Leo <lstudart@pyx4.com>
2023-10-18 11:01:01 -0400

    feat: add filters to cities page and pass searchParams as props

--- src/app/cities/page.tsx
+++ src/app/cities/page.tsx
@@ -1,11 +1,23 @@
 import { getAllCities } from '@core/main/api/cities'
 import { CitiesList } from "components/Cities/CitiesList"
+import { Filters } from 'components/Filters'
+import MaxWidthWrapper from 'components/MaxWidthWrapper'
 
-async function Cities() {
-  const cities = await getAllCities()
+async function Cities({ searchParams }: {
+  searchParams: {
+    [key: string]: string
+  }
+}) {
+
+  const cities = await getAllCities(searchParams)
 
   return (
-    <CitiesList cities={cities}></CitiesList>
+    <MaxWidthWrapper>
+      <div className='flex flex-col gap-4'>
+        <Filters />
+        <CitiesList cities={cities}></CitiesList>
+      </div>
+    </MaxWidthWrapper>
   )
 }
 
