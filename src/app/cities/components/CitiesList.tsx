0b6e7f773a078b23ccae42905dd9cdbe6d2376ac
Leo <lstudart@pyx4.com>
2023-10-29 13:46:09 -0400

    feat: add Switch to compare cities

--- src/app/cities/components/CitiesList.tsx
+++ src/app/cities/components/CitiesList.tsx
@@ -1,6 +1,11 @@
+'use client'
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import { Switch } from "@/components/ui/switch";
 import { CityModel } from "@core/domain/models";
 import { Card } from "components/Card/Card";
-import Link from "next/link";
+import { useCompareCities } from "context/ComparisonContext";
+import { useRouter } from "next/navigation";
 import { getCitySize, getCost, getLanguages, getWinterSeverity, Tags } from "../helpers";
 
 type CitiesListProps = {
@@ -8,6 +13,10 @@ type CitiesListProps = {
 }
 
 function CitiesList({ cities }: CitiesListProps) {
+
+    const { addCity, selectedCities, removeCity, maxSelection } = useCompareCities()
+    const router = useRouter()
+
     return (
         <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(350px,1fr))]">
             {cities.map(city => {
@@ -35,12 +44,38 @@ function CitiesList({ cities }: CitiesListProps) {
                 const bottomLeft = getWinterSeverity(winter);
                 const bottomRight = getLanguages(language);
                 const topLeft = getCost(costOfLiving, costOverall);
+                const reachedMaxCitiesSelection = selectedCities.length === maxSelection
                 return (
-                    <Link key={id} href={`/cities/${slug}`}>
-                        <Card center={center} title={name} image={imageUrl} bottomLeft={bottomLeft} bottomRight={bottomRight} upperLeft={topLeft}>
-                            <Tags {...restProps} citySize={citySize} />
+                    <div className="cursor-pointer" onClick={() => router.push(`/cities/${slug}`)} key={id}>
+                        <Card
+                            center={center}
+                            title={name}
+                            image={imageUrl}
+                            bottomLeft={bottomLeft}
+                            bottomRight={bottomRight}
+                            upperLeft={topLeft}
+                        >
+                            <div className="flex flex-col gap-4">
+                                <Tags {...restProps} citySize={citySize} />
+                                <Separator />
+                                <div className="flex self-center 
+                                flex-col justify-center items-center gap-2" >
+                                    <Label className="text-2xl  cursor-pointer text-gray-600">Compare {name}</Label>
+                                    {/* TODO: have background red when switched on */}
+                                    <Switch
+                                        className="cursor-grabbing"
+                                        onClick={(e) => {
+                                            e.stopPropagation()
+                                            !selectedCities.includes(name) && !reachedMaxCitiesSelection ? addCity(name) : removeCity(name)
+                                            return
+                                        }}
+                                        disabled={reachedMaxCitiesSelection && !selectedCities.includes(name)}
+                                        checked={selectedCities.includes(name)}
+                                    />
+                                </div>
+                            </div>
                         </Card>
-                    </Link>);
+                    </div>);
             })}
         </div>
     )
