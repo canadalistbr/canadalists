f8f1bbfdd58dc1269f2560f4768603c74c0051bf
Leo <lstudart@pyx4.com>
2023-10-31 17:59:41 -0400

    refactor: use remotePatterns for images. domains is deprecated

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['xdcgqdmfytknfwfiixza.supabase.co']
+    remotePatterns: [{
+      hostname: 'xdcgqdmfytknfwfiixza.supabase.co',
+    }]
   }
 }
 
