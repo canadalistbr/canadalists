eb466636c4313f7ae4604c200057044ee4146d0f
Leo <lstudart@pyx4.com>
2023-11-09 18:09:38 -0500

    style: have larger progress bar with score on center of entire bar

--- src/components/Progressbar/Progressbar.tsx
+++ src/components/Progressbar/Progressbar.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { Label } from '@/components/ui/label'
 import { useEffect, useState } from 'react'
 
 type GradientConfig = {
@@ -55,20 +56,19 @@ export function Progressbar({ progress }: ProgressbarProps) {
 
   return (
     <div
-      className="w-full h-8 bg-gray-200 rounded-2xl"
+      className="w-48 h-6 bg-gradient-to-l from-gray-200 to-gray-400 rounded-2xl relative"
       role="progressbar"
       aria-valuenow={progress}
       aria-valuemin={0}
       aria-valuemax={100}
     >
       <div
-        className={`flex items-center justify-center h-full text-center rounded-2xl bg-gradient-to-l ${backgroundColor} `}
+        className={`absolute top-0 left-0 flex items-center justify-center h-full text-center rounded-2xl bg-gradient-to-l ${backgroundColor} `}
         style={{ width: `${width}%` }}
-      >
-        <span className="text-xl font-extrabold leading-none text-white ">
-          {situation}
-        </span>
-      </div>
+      />
+      <Label className="absolute -translate-x-1/2 left-1/2 -translate-y-1/2 top-1/2 text-medium font-extrabold leading-none text-white ">
+        {situation} - {progress / 10}
+      </Label>
     </div>
   )
 }
